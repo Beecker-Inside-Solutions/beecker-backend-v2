1ff4175af48a6e0aeaa60f77c43a3340bc56fb5b
ArmandoArV <armando.arredondo.valle@gmail.com>
2024-05-26 13:23:05 -0600

    implements retrieval of incident id

--- controller/incidentsController.js
+++ controller/incidentsController.js
@@ -13,6 +13,18 @@ const incidentController = {
         projectID,
         progress,
       } = req.body;
+
+      // Input validation
+      if (
+        !incidentName ||
+        !responsible ||
+        !startDate ||
+        !status.toString() ||
+        !projectID.toString()
+      ) {
+        return res.status(400).json({ message: "Missing required fields" });
+      }
+
       const response = await incidentService.addIncident(
         incidentName,
         responsible,
@@ -24,30 +36,47 @@ const incidentController = {
         progress
       );
 
-      res.status(201).json(response);
+      res.status(201).json({
+        message: "Incident added successfully",
+        incidentID: response.incidentID,
+      });
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res
+        .status(500)
+        .json({ message: "Internal Server Error", error: error.message });
     }
   },
 
   getIncidentByID: async (req, res) => {
     try {
       const { idIncident } = req.params;
+
+      if (!idIncident) {
+        return res.status(400).json({ message: "Incident ID is required" });
+      }
+
       const incident = await incidentService.getIncidentByID(idIncident);
+
+      if (!incident) {
+        return res.status(404).json({ message: "Incident not found" });
+      }
 
       res.status(200).json(incident);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res
+        .status(500)
+        .json({ message: "Internal Server Error", error: error.message });
     }
   },
 
   getAllIncidents: async (req, res) => {
     try {
-      const incident = await incidentService.getAllIncidents();
-
-      res.status(200).json(incident);
+      const incidents = await incidentService.getAllIncidents();
+      res.status(200).json(incidents);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res
+        .status(500)
+        .json({ message: "Internal Server Error", error: error.message });
     }
   },
 
@@ -64,6 +93,18 @@ const incidentController = {
         projectID,
         progress,
       } = req.body;
+
+      if (
+        !idIncident ||
+        !incidentName ||
+        !responsible ||
+        !startDate ||
+        !status.toString() ||
+        !projectID.toString()
+      ) {
+        return res.status(400).json({ message: "Missing required fields" });
+      }
+
       const response = await incidentService.updateIncident(
         idIncident,
         incidentName,
@@ -76,9 +117,17 @@ const incidentController = {
         progress
       );
 
-      res.status(200).json(response);
+      if (response.affectedRows === 0) {
+        return res
+          .status(404)
+          .json({ message: "Incident not found or no change in data" });
+      }
+
+      res.status(200).json({ message: "Incident updated successfully" });
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res
+        .status(500)
+        .json({ message: "Internal Server Error", error: error.message });
     }
   },
 };
--- service/incidents.service.js
+++ service/incidents.service.js
@@ -1,50 +1,89 @@
 const connection = require("../helpers/mysql-config");
 
 const incidentService = {
-    addIncident: async (incidentName = "", responsible = "", startDate = "", endDate = "", status = "", description = "", projectID = 0, progress = "") => {
-        try {
-            const query = "INSERT INTO Incidents (incidentName, responsible, startDate, endDate, status, description, isActive, Project_idProject, progress) VALUES (?, ?, ?, ?, ?, ?, 1, ?, ?)";
-            const [rows] = await connection.query(query, [incidentName, responsible, startDate, endDate, status, description, projectID, progress]);
+  addIncident: async (
+    incidentName = "",
+    responsible = "",
+    startDate = "",
+    endDate = "",
+    status = "",
+    description = "",
+    projectID = 0,
+    progress = ""
+  ) => {
+    try {
+      const query =
+        "INSERT INTO Incidents (incidentName, responsible, startDate, endDate, status, description, isActive, Project_idProject, progress) VALUES (?, ?, ?, ?, ?, ?, 1, ?, ?)";
+      const [result] = await connection.query(query, [
+        incidentName,
+        responsible,
+        startDate,
+        endDate,
+        status,
+        description,
+        projectID,
+        progress,
+      ]);
 
-            return rows;
-        } catch (error) {
-            throw error;
-        }
-    },
-
-    getIncidentByID: async (incidentID = "") => {
-        try {
-            const query = "SELECT * FROM Incidents WHERE idIncident = ?";
-            const [rows] = await connection.query(query, [incidentID]);
-
-            return rows;
-        } catch (error) {
-            throw error;
-        }
-    },
-
-    getAllIncidents: async () => {
-        try {
-            const query = "SELECT * FROM Incidents";
-            const [rows] = await connection.query(query);
-
-            return rows;
-        } catch (error) {
-            throw error;
-        }
-    },
-
-    updateIncident: async (incidentID = "", incidentName = "", responsible = "", startDate = "", endDate = "", status = "", description = "", progress = "") => {
-        try {
-            const query = "UPDATE Incidents SET incidentName = ?, responsible = ?, startDate = ?, endDate = ?, status = ?, description = ?, progress = ? WHERE idIncident = ?";
-            const [rows] = await connection.query(query, [incidentName, responsible, startDate, endDate, status, description, progress, incidentID]);
-
-            return rows;
-        } catch (error) {
-            throw error;
-        }
+      return {
+        incidentID: result.insertId,
+      };
+    } catch (error) {
+      throw error;
     }
+  },
+
+  getIncidentByID: async (incidentID = "") => {
+    try {
+      const query = "SELECT * FROM Incidents WHERE idIncident = ?";
+      const [rows] = await connection.query(query, [incidentID]);
+
+      return rows[0]; // Assuming you want to return the first incident if found
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  getAllIncidents: async () => {
+    try {
+      const query = "SELECT * FROM Incidents";
+      const [rows] = await connection.query(query);
+
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  updateIncident: async (
+    incidentID = "",
+    incidentName = "",
+    responsible = "",
+    startDate = "",
+    endDate = "",
+    status = "",
+    description = "",
+    progress = ""
+  ) => {
+    try {
+      const query =
+        "UPDATE Incidents SET incidentName = ?, responsible = ?, startDate = ?, endDate = ?, status = ?, description = ?, progress = ? WHERE idIncident = ?";
+      const [result] = await connection.query(query, [
+        incidentName,
+        responsible,
+        startDate,
+        endDate,
+        status,
+        description,
+        progress,
+        incidentID,
+      ]);
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  },
 };
 
 module.exports = incidentService;
-
