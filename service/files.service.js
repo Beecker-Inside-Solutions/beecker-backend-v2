45cf3251344972b467cbc13ab04dbde98aa5da51
ArmandoArV <armando.arredondo.valle@gmail.com>
2024-05-27 22:17:07 -0600

    adds base64

--- service/files.service.js
+++ service/files.service.js
@@ -1,49 +1,53 @@
 const connection = require("../helpers/mysql-config");
 
 const fileService = {
-    addFile: async (fileData, incidentID) => {
-        try {
-            const query = "INSERT INTO Files (file, Incidents_idIncident) VALUES (?, ?)";
-            const [rows] = await connection.query(query, [fileData, incidentID]);
+  addFile: async (fileData, incidentID) => {
+    try {
+      // Encode fileData to base64
+      const base64Data = fileData.toString("base64");
 
-            return rows;
-        } catch (error) {
-            throw error;
-        }
-    },
+      const query =
+        "INSERT INTO Files (file, Incidents_idIncident) VALUES (?, ?)";
+      const [rows] = await connection.query(query, [base64Data, incidentID]);
 
-    getFileByID: async (fileID) => {
-        try {
-            const query = "SELECT * FROM Files WHERE idFiles = ?";
-            const [rows] = await connection.query(query, [fileID]);
-
-            return rows;
-        } catch (error) {
-            throw error;
-        }
-    },
-
-    getAllFiles: async () => {
-        try {
-            const query = "SELECT * FROM Files";
-            const [rows] = await connection.query(query);
-
-            return rows;
-        } catch (error) {
-            throw error;
-        }
-    },
-
-    deleteFile: async (fileID) => {
-        try {
-            const query = "DELETE FROM Files WHERE idFiles = ?";
-            const [rows] = await connection.query(query, [fileID]);
-
-            return rows;
-        } catch (error) {
-            throw error;
-        }
+      return rows;
+    } catch (error) {
+      throw error;
     }
+  },
+
+  getFileByID: async (fileID) => {
+    try {
+      const query = "SELECT * FROM Files WHERE idFiles = ?";
+      const [rows] = await connection.query(query, [fileID]);
+
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  getAllFiles: async () => {
+    try {
+      const query = "SELECT * FROM Files";
+      const [rows] = await connection.query(query);
+
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  deleteFile: async (fileID) => {
+    try {
+      const query = "DELETE FROM Files WHERE idFiles = ?";
+      const [rows] = await connection.query(query, [fileID]);
+
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  },
 };
 
 module.exports = fileService;
